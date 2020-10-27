3e5def1860983bfe68a291ab61bf1410d663f5e0
CaeDe <y.miyazaki2222@gmail.com>
2020-10-27 15:00:19 +0900

    first commit

---
+++ ruby_lessons/hello.rb
@@ -0,0 +1,12 @@
+=begin
+コメント
+コメント
+コメント
+コメント
+コメント
+=end
+
+print "hello world"
+puts "hello world" # +改行
+p "hello world" # デバッグ用
+ 
---
+++ ruby_lessons/practice.rb
@@ -0,0 +1,23 @@
+#変数
+# - 英小文字 -
+
+msg = "hello world"
+puts msg
+
+msg = "hello world agein"
+puts msg
+
+# 定数
+# - 英大文字
+
+VERSION = 1.1
+puts VERSION
+
+VERSION = 1.2
+puts VERSION
+
+# 数値
+# 32 4.8
+
+p 4.8.class
+p 4.8.methods
