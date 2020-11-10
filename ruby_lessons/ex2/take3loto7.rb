6600c41ec9cda2b7a1aa6345c678328285fc6ee1
CaeDe <y.miyazaki2222@gmail.com>
2020-11-10 13:56:42 +0900

    今度こそfix

--- ruby_lessons/ex2/take3loto7.rb
+++ ruby_lessons/ex2/take3loto7.rb
@@ -1,12 +1,16 @@
 #1~31の数字の配列を作成
+#*によって引数を複数個設定できる
+#引数は配列として受け取られる
 array_num = [*1..31]
+
+#配列の個数を数える
+#||はブロックパラメータ　繰り返しが何回かを知ることができる
 array_num.count.times do |num|
-#7.times do
 
-#ランダムにとった要素を一つずつ削除してい
-
+#ランダムにとった配列の要素を削除していき、削除した要素を表示
 print array_num.delete_at(rand(array_num.count))  
 print " "
+#numブロックの処理の繰り返し回数が６回になったら終了
 break if num == 6
 end
 
