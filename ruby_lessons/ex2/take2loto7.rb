deabf94e35b81cabf8874e5bddd2aaec3787c1df
CaeDe <y.miyazaki2222@gmail.com>
2020-11-11 15:22:47 +0900

    fix

--- ruby_lessons/ex2/take2loto7.rb
+++ ruby_lessons/ex2/take2loto7.rb
@@ -1,13 +1,17 @@
-7.times do
-#1~31の数字をランダムに一個とる
-num = rand.in_a("1 ".."31 ")
-
-  print num
-end 
+#結果の格納する配列
+loto_array = []
+#1から31の配列
+#num_array = [*1..31]
+#loto_arrayの要素が７つ入るまで処理を繰り返す
+while loto_array.size < 7 do
+  #1~31の数字をランダムにとる　
+  temp = [*1..31].sample
+  #もしloto_arrayを検索して、tempがなくfalseが返ってきたら
+  #loto_arrayにtempを追加
+  if loto_array.include?(temp) == false then
+    loto_array.push(temp)
+  end
+end
+#配列じゃない形で表示
+print loto_array.join(" ")
 print "\n"
-
-#今までとった数字と重複がないか確認
-
-
-#最初の7字を表示
-
